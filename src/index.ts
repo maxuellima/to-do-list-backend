244f3f9c14cf0b92924741dd03166678efd198ea
Maxuel Lima <maxuellima20@gmail.com>
2024-01-28 16:56:08 -0300

    Delete user implementado

--- src/index.ts
+++ src/index.ts
@@ -127,3 +127,40 @@ app.post("/users", async (req: Request, res: Response) => {
         }
     }
 })
+
+//DELETE/users
+app.delete("/users/:id", async (req: Request, res: Response) => {
+    try {
+        const idToDelete = req.params.id;
+
+        if(idToDelete[0] !== "f"){
+            res.status(400)
+            throw new Error("'id' deve iniciar com a letra 'f '")
+        }
+
+
+        const [userIdAlreadyExists]: TUserDB[] | undefined[] = await db("users").where({id: idToDelete})
+      
+        
+        if(!userIdAlreadyExists) {
+            res.status(400)
+            throw new Error("'id' não existe! Digite um 'id' válido")
+
+        }
+        await db("users").del().where({id: idToDelete})
+        res.status(200).send({message: "User deletado com sucesso"})
+
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
