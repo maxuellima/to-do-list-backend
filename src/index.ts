8ab9c1c19c7e0d32184204649b03f7622560e6cc
Maxuel Lima <maxuellima20@gmail.com>
2024-01-28 16:27:32 -0300

    Criei o git post do endpoint user

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { db } from './database/knex'
+import { TUserDB } from './type'
 
 const app = express()
 
@@ -39,6 +40,79 @@ app.get("/users", async (req: Request, res: Response) => {
             const result = await db("users").where("name", "LIKE", `%${searchTerm}%`)
             res.status(200).send(result)
         }     
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
+
+app.post("/users", async (req: Request, res: Response) => {
+    try {
+        const {id, name, email, password} = req.body
+
+        if(typeof id !== "string"){
+            res.status(400)
+            throw new Error("'id' deve ser string")
+        }
+        if(id.length < 4){
+            res.status(400)
+            throw new Error("'id' deve possuir pelo menos 4 caracteres")
+        }
+        
+        if(typeof name !== "string"){
+            res.status(400)
+            throw new Error("'name' deve ser string")
+        }
+        if(name.length < 2){
+            res.status(400)
+            throw new Error("'name' deve possuir pelo menos 2 caracteres")
+        }
+
+        if(typeof email !== "string"){
+            res.status(400)
+            throw new Error("'email' deve ser string")
+        }
+        if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,12}$/g)) {
+			res.status(400)
+            throw new Error("'password' deve possuir entre 8 e 12 caracteres, com letras maiúsculas e minúsculas e no mínimo um número e um caractere especial")
+		}
+
+        const [userIdAlreadyExists]: TUserDB[] | undefined = await db("users").where({id})
+
+        if(userIdAlreadyExists){
+            res.status(400)
+            throw new Error("'id' já existe, crie com um id diferente")
+        }
+
+        const [userEmailAlreadyExists]: TUserDB[] | undefined = await db("users").where({email})
+
+        if(userEmailAlreadyExists){
+            res.status(400)
+            throw new Error("'email' já existe, faça o login ou cadastre um email diferente")
+        }
+
+        const newUser: TUserDB = {
+            id,
+            name,
+            email,
+            password
+        }
+
+        await db("users").insert(newUser)
+
+        res.status(201).send({message: "User criado com sucesso", user: newUser})
+
+
+
     } catch (error) {
         console.log(error)
 
