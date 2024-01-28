fe4862e8610d37881e280f1eaeced09ede8e7e55
Maxuel Lima <maxuellima20@gmail.com>
2024-01-28 12:53:23 -0300

    Criando o método GetAllUsers e uma query de busca pelo nome

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,32 @@ app.listen(3003, () => {
 
 app.get("/ping", async (req: Request, res: Response) => {
     try {
-        const result = await db("users")
         res.status(200).send({ message: "Pong!"})
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
+app.get("/users", async (req: Request, res: Response) => {
+    try {
+        const searchTerm = req.query.name as string | undefined;
+
+        if(searchTerm === undefined){            
+            const result = await db("users")
+            res.status(200).send(result)
+        }else{
+            const result = await db("users").where("name", "LIKE", `%${searchTerm}%`)
+            res.status(200).send(result)
+        }     
     } catch (error) {
         console.log(error)
 
